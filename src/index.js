c3067c97c98c4f2a4a70d681141d44c4da203512
Dima Paymushkin <Dimon-94@yandex.ru>
2020-10-16 05:02:31 +0400

    feat: complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+   n = Math.abs(n);
+   //return parseInt(n.toString().split('').reverse().join(''));
+   let res = 0;
+   while(n){
+      res = res * 10 + n % 10;
+      n = Math.floor(n / 10);
+   }
+   return res;
 }
